c8d3382c02cc352e85724c7f6ab85cb9925bfcdb
Matthew Mallimo <Matthew.Mallimo@gmail.com>
2017-08-12 22:21:43 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 var {mongoose} =  require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+const {ObjectID} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -26,6 +26,23 @@ app.get('/todos', (req, res) => {
 	}, (err) => {
 		res.send(err);
 	});
+});
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(ObjectID.isValid(id)){
+		Todo.findById(id).then( (todo) => {
+			if(todo){
+				res.status(200).send(todo);
+			} else {
+				res.status(404).send();
+			}
+		}, (err) => {
+			res.status(400).send();
+		});
+	} else {
+		res.status(400).send();
+	}
 });
 
 app.listen(3000, () => {
